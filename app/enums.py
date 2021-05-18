71aef250e3cebb130776e08330c45a95f0dc5273
Sơn Shadow <64057133+sonVT1999@users.noreply.github.com>
2021-05-18 17:51:27 +0700

    sửa đường dẫn ảnh

--- app/enums.py
+++ app/enums.py
@@ -2,6 +2,6 @@
 
 SUPER_ADMIN_ID = ""
 UPLOAD_FOLDER = 'app/files/images_items'
-SAVE_IMAGE_ITEM = '192.168.1.250:5012/images_items'
+SAVE_IMAGE_ITEM = 'http://192.168.1.250:5012/images_items'
 UPLOAD_FOLDER2 = 'app/files/images_recipes'
-SAVE_IMAGE_RECIPE = '192.168.1.250:5012/images_recipes'
+SAVE_IMAGE_RECIPE = 'http://192.168.1.250:5012/images_recipes'
